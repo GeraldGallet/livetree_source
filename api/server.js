3a56493fc06fccaa2c9b2d675e75f91b7ebeedd2
Gérald Gallet <gerald.gallet@isen-lille.fr>
2018-03-28 10:34:12 +0200

    Le dossier API contient un exemple de chaque type de requete fonctionnel

--- api/server.js
+++ api/server.js
@@ -5,6 +5,9 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+
 app.listen(port);
 
 //Database connection
@@ -23,9 +26,20 @@ app.get('/', function(req, res, next) {
   res.send('hello world :)');
 });
 
-app.get('/user/all', function(req, res, next) {
-  console.log('/user/all');
+/*app.get('/user', function(req, res, next) {
+  console.log('GET /user');
   var query = 'SELECT * FROM user;';
+
+  res.locals.connection.query(query, function(error, results, fields) {
+    if(error) throw error;
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+  });
+});*/
+
+app.post('/get_user', jsonParser, function(req, res, next) {
+  console.log("POST /get_user");
+  var data = req.body;
+  var query = 'SELECT * FROM user WHERE email = \'' + data.email + '\';';
 
   res.locals.connection.query(query, function(error, results, fields) {
     if(error) throw error;
@@ -33,13 +47,35 @@ app.get('/user/all', function(req, res, next) {
   });
 });
 
-app.get('/user/:mail', function(req, res, next) {
-  console.log('/user/' + req.params.mail);
-  var query = 'SELECT * FROM user WHERE email = \'' + req.params.mail + '\';';
+app.post('/user', jsonParser, function (req, res) {
+  console.log('POST /user');
+  var postData  = req.body;
+
+  res.locals.connection.query('INSERT INTO user SET ?', postData, function (error, results, fields) {
+    if (error) throw error;
+    res.end(JSON.stringify(results));
+  });
+});
+
+app.patch('/user/password', jsonParser, function(req, res) {
+  console.log("PATCH /user/password");
+  var data = req.body;
+  var query = "UPDATE user SET password = \'" + data.password + '\' WHERE email = \'' + data.email + '\';';
+
+  res.locals.connection.query(query, function (err, result) {
+    if (err) throw err;
+    console.log(result.affectedRows + " record(s) updated");
+  });
+});
+
+app.delete('/user', jsonParser, function(req, res, next) {
+  console.log("DELETE /user");
+  var data = req.body;
+  var query = 'DELETE FROM user WHERE email = \'' + data.email + '\'';
 
   res.locals.connection.query(query, function(error, results, fields) {
     if(error) throw error;
-    console.log(results);
-    res.send(JSON.stringify({"status": 200, "error": null, "response": JSON.stringify(results)}));
+    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
   });
+
 });
--- api/test_api.php
+++ api/test_api.php
@@ -1,10 +1,79 @@
 <?php
   echo "Hello TEST API ;)\n";
-  $mail = "gerald.gallet@isen.yncrea.fr";
-  $response = (array) json_decode(file_get_contents('http://localhost:3000/user/' . $mail));
-  //$results = (array) json_decode($response["response"]);
-  $results = json_decode($response["response"], true);
-  $results = $results[0];
-  echo $results["first_name"];
-  //echo $results;
+  $api_url = 'http://localhost:3000/';
+  $mail = "alexis.fardel@isen.yncrea.fr";
+
+  // GET EXAMPLE
+  /*
+  $ch = curl_init($api_url . 'get_user/');
+  $getData = array(
+    'email' => 'robin.poiret@isen.yncrea.fr'
+  );
+
+  //Encode the array into JSON.
+  $getDataEncoded = json_encode($getData);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $getDataEncoded);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+  $data = curl_exec($ch);
+  $data = json_decode($data, true);
+  $user = $data['response'][0];
+
+  var_dump($user);
+  //*/
+
+  // POST EXAMPLE
+  /*
+  $ch = curl_init($api_url . 'user');
+  $postData = array(
+    'email' => 'robin.poiret@isen.yncrea.fr',
+    'first_name' => 'Robin',
+    'last_name' => 'Poiret',
+    'password' => 'passw0rdR',
+    'phone_number' => '0606060606',
+    'id_status' => 'student'
+  );
+  $postDataEncoded = json_encode($postData);
+
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postDataEncoded);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  $result = curl_exec($ch);
+  //*/
+
+  // DELETE EXAMPLE
+  ///*
+  $ch = curl_init($api_url . 'user/');
+  $postData = array(
+    'email' => 'robin.poiret@isen.yncrea.fr'
+  );
+  $postDataEncoded = json_encode($postData);
+
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postDataEncoded);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+
+  $result = curl_exec($ch);
+  //*/
+
+  // PATCH EXAMPLE
+  /*
+  $ch = curl_init($api_url . 'user/password');
+  $postData = array(
+    'email' => 'robin.poiret@isen.yncrea.fr',
+    'password' => 'passw0rd2lafamile'
+  );
+  $postDataEncoded = json_encode($postData);
+
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PATCH');
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $postDataEncoded);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  $result = curl_exec($ch);
+  curl_close($ch);
+  //*/
 ?>
