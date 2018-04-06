ec63bed73dc06f7d861663076f620452d2867d9c
Gérald Gallet <gerald.gallet@isen-lille.fr>
2018-04-06 15:56:26 +0200

    Page de Planning ajoutée

--- src/Controller/Pages/Planning.php
+++ src/Controller/Pages/Planning.php
@@ -11,12 +11,41 @@
       * @Route("/planning")
       */
     public function load_planning() {
-      $first_name = "Gérald";
-      $last_name = "Gallet";
+      $object = "de la voiture";
+      $time1 = array(
+        'first' => 0,
+        'second' => 2
+      );
+      $time2 = array(
+        'first' => 10,
+        'second' => 12
+      );
+      $time3 = array(
+        'first' => 14,
+        'second' => 18
+      );
+      $date = [$time1, $time2, $time3];
+      $dates = array(
+        '02/04' => $date,
+        '03/04' => $date,
+        '04/04' => $date,
+        '05/04' => $date,
+        '06/04' => $date,
+        '07/04' => $date,
+        '08/04' => $date
+      );
+
       return $this->render('reservations/planning.html.twig', array(
-            'first_name' => $first_name,
-            'last_name' => $last_name
+            'object' => $object,
+            'dates' => $dates
       ));
+    }
+
+    /**
+      * @Route("/back", name="go_back")
+      */
+    public function go_back() {
+      // Go back to the current reservation
     }
   }
 
