9a876fd98ba12de6a08041e48548fffce3b8eba6
Gérald Gallet <gerald.gallet@isen-lille.fr>
2018-04-11 09:36:14 +0200

    Classe testmail dans un Controller

--- src/Controller/Pages/testmail.php
+++ src/Controller/Pages/testmail.php
@@ -1,50 +1,55 @@
 <?php
 
+  namespace App\Controller\Pages;
+
+  use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+  use Symfony\Component\HttpFoundation\Request;
+  use Symfony\Component\HttpFoundation\Response;
+  use Symfony\Component\Routing\Annotation\Route;
+
+  class testmail {
+
+    /**
+      * @Route("/test", name="test_mail")
+      */
+    function load_test() {
+
+      $mail = 'coelablivetree@gmail.com'; // Déclaration de l'adresse de destination.
+
+      if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
+      {
+        $passage_ligne = "\r\n";
+      } else
+      {
+        $passage_ligne = "\n";
+      }
+
+      //=====Déclaration des messages au format texte et au format HTML.
 
 
+      $message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
+      $header = "From: \"livetree\"<mailsender59270@gmail.com>".$passage_ligne;
+      $header.= "Reply-to: \"livetree\" <mailsender59270@gmail.com".$passage_ligne;
+      $header.= "MIME-Version: 1.0".$passage_ligne;
+      $header.= "Content-type: text/html; charset=utf8;".$passage_ligne;
+      $sujet = "test";
 
+      $message= $passage_ligne.$message_html.$passage_ligne;
+      if(mail($mail, $sujet, $message, $header))
+      {
+        echo "mail envoyer ";
+      } else
+      {
+        echo "pasbon ";
+      }
+      /*if(mail("mailsender59270@gmail.com", "allo", "j'essaie")){
+        echo "mail2 envoyer";
+      } else
+      {
+        echo "pasbon2";
+      }*/
 
+    }
+  }
 
-$mail = 'coelablivetree@gmail.com'; // Déclaration de l'adresse de destination.
-
-if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
-
-{
-
-    $passage_ligne = "\r\n";
-
-}
-
-else
-
-{
-
-    $passage_ligne = "\n";
-
-}
-
-//=====Déclaration des messages au format texte et au format HTML.
-
-
-$message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
-$header = "From: \"livetree\"<mailsender59270@gmail.com>".$passage_ligne;
-$header.= "Reply-to: \"livetree\" <mailsender59270@gmail.com".$passage_ligne;
-$header.= "MIME-Version: 1.0".$passage_ligne;
-$header.= "Content-type: text/html; charset=utf8;".$passage_ligne;
-$sujet = "test";
-
-$message= $passage_ligne.$message_html.$passage_ligne;
-if(mail($mail, $sujet, $message, $header)){
-echo "mail envoyer ";
-}
-else{
-  echo "pasbon ";
-}
-/**if(mail("mailsender59270@gmail.com", "allo", "j'essaie")){
-echo "mail2 envoyer";
-}
-else{
-  echo "pasbon2";
-}*/
-
- ?>
+  ?>
