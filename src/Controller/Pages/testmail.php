13855b356f19f89e77a506e82323db561ed215cc
unknown <khanda59270@gmail.com>
2018-04-11 17:25:58 +0200

    email

--- src/Controller/Pages/testmail.php
+++ src/Controller/Pages/testmail.php
@@ -1,5 +1,5 @@
 <?php
-
+// composer require mailer
   namespace App\Controller\Pages;
 
   use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -7,49 +7,40 @@
   use Symfony\Component\HttpFoundation\Response;
   use Symfony\Component\Routing\Annotation\Route;
 
-  class testmail {
+  class testmail extends Controller{
 
     /**
       * @Route("/test", name="test_mail")
       */
-    function load_test() {
-
-      $mail = 'coelablivetree@gmail.com'; // Déclaration de l'adresse de destination.
-
-      if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $mail)) // On filtre les serveurs qui présentent des bogues.
+      public function indexAction( \Swift_Mailer $mailer)
       {
-        $passage_ligne = "\r\n";
-      } else
-      {
-        $passage_ligne = "\n";
+        $name='robin';
+        echo "$name";
+          $message = (new \Swift_Message('Hello Email'))
+              ->setFrom('mailsender59270@gmail.com')
+              ->setTo('mailsender59270@gmail.com')
+              ->setBody(
+                  $this->renderView(
+                      // templates/emails/registration.html.twig
+                      'test_mail.html.twig',
+                      array('name' => $name)
+                  ),
+                  'text/html'
+              )
+
+          ;
+
+        if ($mailer->send($message))
+        {
+          echo "done";
+        }else
+        {
+          echo "fail";
+        }
+
+
+          return $this->render('test_mail.html.twig');
       }
-
-      //=====Déclaration des messages au format texte et au format HTML.
-
-
-      $message_html = "<html><head></head><body><b>Salut à tous</b>, voici un e-mail envoyé par un <i>script PHP</i>.</body></html>";
-      $header = "From: \"livetree\"<mailsender59270@gmail.com>".$passage_ligne;
-      $header.= "Reply-to: \"livetree\" <mailsender59270@gmail.com".$passage_ligne;
-      $header.= "MIME-Version: 1.0".$passage_ligne;
-      $header.= "Content-type: text/html; charset=utf8;".$passage_ligne;
-      $sujet = "test";
-
-      $message= $passage_ligne.$message_html.$passage_ligne;
-      if(mail($mail, $sujet, $message, $header))
-      {
-        echo "mail envoyer ";
-      } else
-      {
-        echo "pasbon ";
-      }
-      /*if(mail("mailsender59270@gmail.com", "allo", "j'essaie")){
-        echo "mail2 envoyer";
-      } else
-      {
-        echo "pasbon2";
-      }*/
-
-    }
   }
 
   ?>
