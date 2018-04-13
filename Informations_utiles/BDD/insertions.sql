9034f777bf4e2d903fd81bf31ba0dedf45b0ec9a
mabooyaDlespace <simonnetjc@hotmail.fr>
2018-04-13 11:46:03 +0200

    node modules

--- Informations_utiles/BDD/insertions.sql
+++
@@ -1,7 +0,0 @@
-USE `projet_LiveTree`;
-
-insert into status(id_status,rights)
-values('teacher',1);
-
-insert into user(email,password,phone_number,first_name,last_name,activated,id_status)
-values ('aa@htt.fr','abcdef',09876543212,'jc','sim',true,'teacher');
