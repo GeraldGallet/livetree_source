ca2024394bc57a9e29ba956c9eec3c2a38ea31b6
mabooyaDlespace <simonnetjc@hotmail.fr>
2018-04-13 15:56:08 +0200

    Check Available time

--- src/Controller/Form/AvailableTime.php
+++ src/Controller/Form/AvailableTime.php
@@ -8,10 +8,12 @@
 
 namespace App\Controller\Form;
 
-use App\Controller\jc\Pair;
+use DateInterval;
+use DateTime;
 use Symfony\Component\Routing\Annotation;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Controller\CustomApi;
+
 class AvailableTime
 {
     public static $listedesreservation;
@@ -21,37 +23,53 @@ class AvailableTime
      */
     function aa()
     {
+        $TimeDivision=24;
         $api_interface = new CustomApi();
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T17:03:01'));
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T17:03:01'));
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T17:03:01'));
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T20:03:01'));
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T20:03:01'));
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T20:03:01'));
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T21:03:01'));
+        $listedesreservation[] = array('min' => new DateTime('2011-01-01T15:03:01'), 'max' => new DateTime('2011-01-01T23:03:01'));
 
-        $listedesreservation[]= array('min'=>2,'max'=> 4);
-        $listedesreservation[] = array('min'=>2,'max'=> 4);
-        $listedesreservation[] = array('min'=>2,'max'=> 4);
-        $listedesreservation[] = array('min'=>2,'max'=> 5);
-        $listedesreservation[] = array('min'=>2,'max'=> 5);
-        $listedesreservation[] = array('min'=>2,'max'=> 5);
-        $listedesreservation[] = array('min'=>2,'max'=> 7);
-        $listedesreservation[] = array('min'=>2,'max'=> 10);
-
-
+        //init tab
+        $startingTimeOfTheDay = new DateTime('2011-01-01T00:00:00');
         $array = array();
-        for ($i = 1; $i <= 11; $i++) {
-            $array[] = 0;
+        $CRENNEAU = array();
+        for ($i = 1; $i <= $TimeDivision; $i++) {
+            $tmp= clone $startingTimeOfTheDay;
+            $CRENNEAU[]=array('date'=>$tmp,'numberOfDisponibility'=>0);
+
+            $startingTimeOfTheDay->add(new DateInterval('P0Y0DT1H0M'));
         }
-        dump($array);
+        dump($CRENNEAU);
         foreach ($listedesreservation as $tuple) {
             $max = $tuple['max'];
             $min = $tuple['min'];
 
-            for ($i = 1; $i <= 10; $i++) {
-                if ($min <= $i && $max >= $i) {
-                    $array[$i] +=1;
+            foreach ($CRENNEAU as &$timeNdisp){
+                if ($min <= $timeNdisp['date'] && $max >= $timeNdisp['date']) {
+                    $mmtemp =$timeNdisp['numberOfDisponibility'];
+                    $mmtemp+=1;
+                    $timeNdisp['numberOfDisponibility']=$mmtemp;
+                    dump($timeNdisp['numberOfDisponibility'],$mmtemp);
+                    /*
+                    dump($min <= $timeNdisp['date'] && $max >= $timeNdisp['date']);*/
+
                 }
             }
         }
-        dump($array);
+        dump($listedesreservation);
+        dump($CRENNEAU);
+        //$bornes = $api_interface->reservation_borne_get_all();
+        //dump($bornes);
 
-        $bornes=$api_interface->reservation_borne_get_all();
-        dump($bornes);
+        $datetime1 = new DateTime('2009-10-11');
+        $datetime2 = new DateTime('2009-10-13');
+        $interval = $datetime1->diff($datetime2);
+        echo $interval->format('%R%a days');
         return new JsonResponse($array);
     }
 
